650b0783b16942e342a19ade2491645395569996
Dennis Hernandez <dennis264@mac.com>
2019-09-30 12:08:20 -0700

    Initial Commit

---
+++ Schwifty/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Schwifty
+//
+//  Created by Dennis Hernandez on 9/30/19.
+//  Copyright © 2019 Dennis Hernandez. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Schwifty/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Schwifty
+//
+//  Created by Dennis Hernandez on 9/30/19.
+//  Copyright © 2019 Dennis Hernandez. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
